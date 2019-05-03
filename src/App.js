9ed9d5dab871170213867e30ff11a2c2db3b737a
anderjs <andersonav37@gmail.com>
2019-05-03 16:21:59 -0400

    Fix path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Landing from './modules/landing/landing'
+import LandingView from './modules/landing/landing'
 import AboutUsView from './modules/about-us/AboutUSView'
 import Faq from './modules/faq/faq'
 import Plan from './modules/plan/plan'
@@ -9,7 +9,7 @@ import Terms from './modules/terms/terms'
 import SucessView from './modules/register/SucessView';
 
 const routes = [
-  { component: Landing, path: '/' },
+  { component: LandingView, path: '/' },
   { component: SucessView, path: '/success' },
   { component: Terms, path: '/terms-and-conditions' },
   { component: Plan, path: '/plan' },
