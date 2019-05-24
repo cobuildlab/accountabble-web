772c0540ede78e4672803fa8d9d8a7652c96c787
anderjs <andersonav37@gmail.com>
2019-05-24 13:12:42 -0400

    Hot fix plan

---
+++ src/modules/plan/Plan-2.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { MDBRow, MDBContainer, MDBCol} from 'mdbreact'
+import { Link } from 'react-router-dom'
+
+import {Icon} from 'react-icons-kit';
+import {thinLeft} from 'react-icons-kit/entypo/thinLeft';
+import Navbar from '../../components/Navbar';
+import FooterComponent from '../../components/FooterComponent';
+import StepperPlan from '../../components/StepperPlan';
+import '../../assets/scss/style.scss';
+
+const Plan = () => {
+  return(
+    <React.Fragment>
+      <Navbar/>
+        <div className="secction-view">
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="12">
+                <h1 className="text-center text-white title-plan pb-4">
+                <span className="text-white float-left">
+                  <Link className="link-nav ml-4" to="/">
+                    <Icon icon={ thinLeft } size="38" />
+                  </Link>
+                </span>
+                  Plan
+                </h1>
+                <StepperPlan />
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
+      <FooterComponent/>
+    </React.Fragment>
+  );
+}
+export default Plan;
