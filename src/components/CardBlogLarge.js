751d5a1d8687eb664bbccf5773c3f3508ba3f8a3
Cesar Morales <david_avid1@hotmail.com>
2019-05-28 16:05:28 -0400

    register

--- src/components/CardBlogLarge.js
+++ src/components/CardBlogLarge.js
@@ -1,40 +1,52 @@
 import React from "react";
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdbreact';
-import PropTypes from 'prop-types';
-import '../assets/scss/style.scss'
-import { parseStringIntoDOM, getSourceAttribute, getPreviewText } from  '../utils/parser';
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText
+} from "mdbreact";
+import PropTypes from "prop-types";
+import "../assets/scss/style.scss";
+import {
+  parseStringIntoDOM,
+  getSourceAttribute,
+  getPreviewText
+} from "../utils/parser";
 import BlogImage from "./BlogImage";
 
 const CardBlogLarge = ({ blogTtile, content, author, published }) => {
-  const div = parseStringIntoDOM(content).html('div');
+  const div = parseStringIntoDOM(content).html("div");
   // const text = getPreviewText('div', div);
-  const anchor  = parseStringIntoDOM(content).html('a');
-  const src = getSourceAttribute('a', anchor);
+  const anchor = parseStringIntoDOM(content).html("a");
+  const src = getSourceAttribute("a", anchor);
 
   function getImageOrDefault() {
-    return src === null ? "https://mdbootstrap.com/img/Photos/Others/images/43.jpg" : src;
-  };
+    return src === null
+      ? "https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+      : src;
+  }
   return (
     <React.Fragment>
       <MDBCard className="section-blog-card mb-4">
-        <div className={'animated fadeIn'}>
+        <div className={"animated fadeIn"}>
           <BlogImage url={getImageOrDefault()} />
         </div>
         <MDBCardBody>
           <MDBCardTitle>{blogTtile}</MDBCardTitle>
-          <MDBCardText>
-          </MDBCardText>
+          <MDBCardText />
           <MDBCardText>
             <small>author: </small>
-            <small className="white-text">
-              {author}
-            </small><br />
+            <small className="white-text">{author}</small>
+            <br />
             <small>published: </small>
             <small className="white-text">
               {new Date(published).toDateString()}
             </small>
           </MDBCardText>
-          <MDBBtn href="#" className="section-blog-btn-blue">Read this post</MDBBtn>
+          <MDBBtn href="#" className="section-blog-btn-blue">
+            Read this post
+          </MDBBtn>
         </MDBCardBody>
       </MDBCard>
     </React.Fragment>
@@ -44,6 +56,5 @@ const CardBlogLarge = ({ blogTtile, content, author, published }) => {
 CardBlogLarge.propTypes = {
   blogTtile: PropTypes.string
 };
-
 
 export default CardBlogLarge;
