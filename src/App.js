7a626b4c3bc3da613358d72d49275c0fef627652
Angel Lacret <alacret@gmail.com>
2019-06-13 11:56:24 -0400

    merge with master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, {Component} from "react";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-
 import LandingView from "./modules/landing/LandingView";
 import AboutUsView from "./modules/about-us/AboutUSView";
 import FaqView from "./modules/faq/FaqView";
 import Plan from "./modules/plan/Plan";
 import TermsView from "./modules/terms/TermsView";
-import SucessView from "./modules/register/SucessView";
+import SuccessView from "./modules/register/SucessView";
+import ContactUsView from "./modules/contact-us/ContactUsView";
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 const routes = [
   {component: LandingView, path: "/"},
-  {component: SucessView, path: "/success"},
+  {component: SuccessView, path: "/success"},
   {component: TermsView, path: "/terms-and-conditions"},
   {component: Plan, path: "/plan"},
   {component: FaqView, path: "/faq"},
   {component: AboutUsView, path: "/about-us"},
-  { component: ContactUsView, path: '/contact-us'}
+  {component: ContactUsView, path: '/contact-us'}
 ];
 
 
